e135eb2b244168a4913c1df68853ea0c52f77823
CHIUBORU <microsoftdeanchiu@gmail.com>
2023-11-21 15:11:40 +0800

    首頁完成囉

--- src/components/IndexApp.vue
+++ src/components/IndexApp.vue
@@ -8,11 +8,52 @@
          <img src="../assets/zoo.png" style="height: 450px; margin-right: 80px;" />
       </div>
    </div>
-   <div style="background-color: #F58D59; margin-bottom: 450px; padding: 20px;">
+   <div style="background-color: #F58D59; padding: 15px 20px 20px 20px;">
+      <center>
+         <p
+            style="color: white; font-weight: bold; font-size: 25px; background-color: #844200; width: 160px; border-radius: 30px; margin-bottom: 15px; line-height: 40px;">
+            動物快照</p>
+      </center>
       <v-carousel cycle>
-         <v-carousel-item src="https://mediaim.expedia.com/destination/1/af240e0c9e8db19e400de545be9c62b1.jpg" cover></v-carousel-item>
-         <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
-         <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+         <v-carousel-item src="https://mediaim.expedia.com/destination/1/af240e0c9e8db19e400de545be9c62b1.jpg"
+            cover></v-carousel-item>
+         <v-carousel-item
+            src="https://www.kkday.com/zh-tw/blog/wp-content/uploads/asia-taiwan-taipei-zooP1590413-%E6%8B%B7%E8%B2%9D.jpg"
+            cover></v-carousel-item>
+         <v-carousel-item
+            src="https://static.gltjp.com/glt/prd/data/directory/12000/11808/20210805_201012_29f412c6_w1920.jpg"
+            cover></v-carousel-item>
       </v-carousel>
+   </div>
+   <div style="height: 350px;">
+      <p style="color: white; font-weight: bold; font-size: 25px; background-color: #844200; width: 40px; border-radius: 30px; height: 220px; padding-top: 15px;
+         margin: 120px 0px 0px 20px; text-align: center;">
+         知識學習區</p>
+   </div>
+   <div style="background-color: #F58D59; padding: 15px 20px 20px 20px;">
+      <center>
+         <p
+            style="color: white; font-weight: bold; font-size: 25px; background-color: #844200; width: 160px; border-radius: 30px; margin-bottom: 15px; line-height: 40px;">
+            聯絡我們</p>
+      </center>
+      <div style="display: grid; grid-template-columns: 1fr 1fr; margin-bottom: 0px;">
+         <div>
+            <iframe
+               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14459.735043571738!2d121.4326549!3d25.0363219!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a7dd8be91eaf%3A0xe342a67d6574f896!2z5aSp5Li75pWZ6LyU5LuB5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1700550019273!5m2!1szh-TW!2stw"
+               width="1200" height="150" style="border:0;"></iframe>
+         </div>
+         <div style="margin-left: 30px; font-weight: bold; line-height: 30px  ;">
+            <p>聯絡窗口：吳娟娟</p>
+            <p>聯絡信箱：ezzoo@gmail.com</p>
+            <p>聯絡電話：0972366816</p>
+            <p>園區營業時間：08:00-17:00</p>
+            <p>窗口聯絡時間：08:30-17:30</p>
+         </div>
+      </div>
+   </div>
+   <div style="background-color: #642100; height: 100px; padding: 36px 0px 0px 0px;">
+      <center>
+         <p style="color: white;">@捐捐不息股份有限公司</p>
+      </center>
    </div>
 </template>
